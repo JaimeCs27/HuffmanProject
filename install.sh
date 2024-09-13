5e5a988f321affeb4c7f3ed5f23acc6587e59a6a
JaimeCs27 <jaimecab2002@gmail.com>
2024-09-12 21:17:10 -0600

    install update

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 sudo dnf install make
 sudo dnf install gcc
+touch Parallel/compress.c
+touch Parallel/decompress.c
+touch Serial/compress.c
+touch Serial/decompress.c
+touch Threads/compress.c
+touch Threads/decompress.c
